0b7cc536f47df3de5fcb1fb01c15fda3ab5f986f
prive <prive@DESKTOP-4QRMNTM>
2024-11-17 15:20:32 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp4/Form3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form3 : WindowsFormsApp4.Form1
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowsFormsApp4/nForm.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class nForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // nForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "nForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
